b0cc30f218a64b36c6f8e12a7b9acc4cee290b7f
Yordan11021986 <yordan.tokuschiev@abv.bg>
2022-05-02 11:08:15 +0300

    Spring-Data-Football-Finder

---
+++ src/test/java/com/example/spring_data_exam_prep/SpringDataExamPrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_data_exam_prep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataExamPrepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
